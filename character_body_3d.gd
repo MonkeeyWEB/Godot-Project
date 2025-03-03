2381788ce9fcbe1be90551643a807d6086e0757a
Aurèle chalumeau <aurele.chalumeau@stfelixlasalle.fr>
2025-03-03 12:05:10 +0100

    bruit de pas

--- character_body_3d.gd
+++ character_body_3d.gd
@@ -1,5 +1,6 @@
 extends CharacterBody3D
 @onready var audio_stream_player_3d: AudioStreamPlayer3D = $CSGBox3D/AudioStreamPlayer3D
+@onready var bruits_pas: AudioStreamPlayer3D = $"bruits pas"
 
 
 const SPEED = 5.0
@@ -14,12 +15,15 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("gauche", "droite", "avancer", "reculer")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
+		if not bruits_pas.playing:
+			bruits_pas.play()
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
