2abb97af2b10eb09ab5a01fbbb647b4a4eb44e6f
Aurèle chalumeau <aurele.chalumeau@stfelixlasalle.fr>
2025-03-12 09:37:12 +0100

    validation de la camera
    oui c dans pick-up

--- character_body_3d.gd
+++ character_body_3d.gd
@@ -1,10 +1,17 @@
 extends CharacterBody3D
 @onready var audio_stream_player_3d: AudioStreamPlayer3D = $CSGBox3D/AudioStreamPlayer3D
 @onready var bruits_pas: AudioStreamPlayer3D = $"bruits pas"
+@onready var camera_3d: Camera3D = $Camera3D
+
 
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const CAMERA_SENS = 0.005
+
+func _ready() -> void:
+	# Initialise le mode de camera à la première personne
+	Input .mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
 func _physics_process(delta: float) -> void:
@@ -21,7 +28,7 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("gauche", "droite", "avancer", "reculer")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
+	if direction :
 		if not bruits_pas.playing:
 			bruits_pas.play()
 		velocity.x = direction.x * SPEED
@@ -31,3 +38,14 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+	
+	
+	#Camera
+func _input(event):
+	if event is InputEventMouseMotion:
+		print("y : ",event.relative.x * CAMERA_SENS)
+		print("x : ",event.relative.y * CAMERA_SENS)
+		#Horizontal
+		rotation.y -= event.relative.x * CAMERA_SENS
+		#Vertical
+		#rotation.x -= event.relative.y * CAMERA_SENS
