e543ba37c76e7a62121a60897e6669927d31de30
Aurèle chalumeau <aurele.chalumeau@stfelixlasalle.fr>
2025-05-19 12:08:28 +0200

    naz

--- Chercheur.gd
+++ Chercheur.gd
@@ -1,6 +1,7 @@
 extends Player
 class_name Chercheur
 
+var Points = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,3 +11,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func pick_up():
+	if Input.is_action_just_pressed("pickup") and hand.is_colliding():
+		if hand.get_collider().is_in_group("fuyard"):
+			hand.get_collider().queue_free()
+			Points += 1
